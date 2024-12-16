30e04af15b750e265ff7cc3abb613fb52f667c54
pierdomenico.reitano <pierdomenico.reitano@farport.co>
2024-12-16 22:19:09 +0100

    feat: add input handling and event

--- src/button/index.js
+++ src/button/index.js
@@ -1,38 +1,37 @@
-import {BaseComponent} from '../base-component';
-import {html} from 'lit';
+import { BaseComponent } from "../base-component";
+import { html } from "lit";
 
-import styles from './main.scss'
-
+import styles from "./main.scss";
 
 export class Button extends BaseComponent {
-
-  static styles = styles
+  static styles = styles;
 
   // Declare reactive properties
   static properties = {
-    type: {type: String},
-    variant: {type: String},
-    _buttonClasses: {type: String},
-    outline: {type: Boolean},
-    disabled: {type: Boolean}
-  
+    type: { type: String },
+    variant: { type: String },
+    _buttonClasses: { type: String },
+    outline: { type: Boolean },
+    disabled: { type: Boolean },
   };
 
   constructor() {
     super();
-    this.type = 'button'
-    this.variant = ''
-    this.outline = false
-    this.disabled = false
+    this.type = "button";
+    this.variant = "";
+    this.outline = false;
+    this.disabled = false;
   }
 
   firstUpdated() {
-    this._buttonClasses = this._composeClass (
-      'btn',
-      this.outline ? '' : this.variant !== '' ? `btn-${this.variant}` : '',
-      this.outline ? `${this.outline ? 'btn-outline-' : ''}${this.variant}` : '',
-      this.disabled ? 'disabled' : '' 
-    )
+    this._buttonClasses = this._composeClass(
+      "btn",
+      this.outline ? "" : this.variant !== "" ? `btn-${this.variant}` : "",
+      this.outline
+        ? `${this.outline ? "btn-outline-" : ""}${this.variant}`
+        : "",
+      this.disabled ? "disabled" : ""
+    );
   }
 
   // Render the UI as a function of component state
@@ -43,7 +42,6 @@ export class Button extends BaseComponent {
       </button>
     `;
   }
-
 }
 
-customElements.define('bsi-button', Button);
+customElements.define("bsi-button", Button);
--- src/input/index.js
+++ src/input/index.js
@@ -1,28 +1,66 @@
-import {BaseComponent} from '../base-component';
-import InputLabel from '../utils/input-label'
-import {html} from 'lit';
+import { BaseComponent } from "../base-component";
+import InputLabel from "../utils/input-label";
+import { html } from "lit";
 
-import styles from './main.scss'
-
+import styles from "./main.scss";
 
 export class Input extends BaseComponent {
-
-  static styles = styles
+  static styles = styles;
 
   // Declare reactive properties
   static properties = {
-    id: '',
-    label: '',
-    type: {type: String}
+    id: "",
+    label: "",
+    type: { type: String },
+    value: { type: String },
   };
 
-  constructor () {
-    super()
-    this.type = 'text'
+  constructor() {
+    super();
+    this.type = "text";
+  }
+
+  updated(changedProperties) {
+    if (changedProperties.has("value")) {
+      if (this.value && this.value.length > 0) {
+        this.requestUpdate();
+      } else {
+        this.requestUpdate();
+      }
+      if (this.value !== this._inputElement.value) {
+        if (this.value) {
+          this._inputElement.value = this.value;
+        } else {
+          this._inputElement.value = "";
+        }
+
+        this.notifyValueChanged();
+      }
+    }
+  }
+
+  notifyValueChanged() {
+    let inputEvent = null;
+
+    inputEvent = new Event("input", {
+      bubbles: true,
+      composed: true,
+    });
+
+    // Dispatched event to alert outside shadow DOM context of event firing.
+    this.dispatchEvent(inputEvent);
   }
 
   firstUpdated() {
-    this._label = new InputLabel(this.renderRoot.querySelector(`input`), this.renderRoot);
+    this._label = new InputLabel(
+      this.renderRoot.querySelector(`input`),
+      this.renderRoot
+    );
+    this._inputElement = this.renderRoot.querySelector("input");
+  }
+
+  handleInput() {
+    this.value = this._inputElement.value;
   }
 
   // Render the UI as a function of component state
@@ -30,11 +68,15 @@ export class Input extends BaseComponent {
     return html`
       <div class="form-group">
         <label for="${this.id}">${this.label}</label>
-        <input type="${this.type}" class="form-control" id="${this.id}">
+        <input
+          @input="${this.handleInput}"
+          type="${this.type}"
+          class="form-control"
+          id="${this.id}"
+        />
       </div>
     `;
   }
-  
 }
 
-customElements.define('bsi-input', Input);
+customElements.define("bsi-input", Input);
