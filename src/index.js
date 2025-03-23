8f2beedf664f0926189835db84a031fa02de2581
Andrea Stagi <stagi.andrea@gmail.com>
2025-03-23 22:44:30 +0100

    feat: add track focus

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ export { BackToTop } from './back-to-top'
 export { Input } from './input'
 export { Button } from './button'
 
+import TrackFocus from './utils/track-focus'
+
 import './global.scss'
+
+new TrackFocus()
