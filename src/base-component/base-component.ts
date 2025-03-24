7de3cb782689d1fad738cb938700443e5e556c6b
Andrea Stagi <stagi.andrea@gmail.com>
2025-03-24 14:43:36 +0100

    feat: add storybook

--- src/base-component/base-component.ts
+++ src/base-component/base-component.ts
@@ -1,11 +1,16 @@
 import {LitElement, unsafeCSS} from 'lit';
+import TrackFocus from '../utils/track-focus.ts'
 
 export const BaseComponent = (style: any) => {
   return class extends LitElement {
 
     static styles = [unsafeCSS(style)];
 
-    _composeClass(...classes: any) {
+    protected addFocus(element: HTMLElement) {
+      new TrackFocus(element)
+    }
+
+    protected composeClass(...classes: any) {
       let composedClass = ''
       classes.forEach((newClass: string) => {
         composedClass += ' ' + newClass
