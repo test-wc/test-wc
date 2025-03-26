c35ddff4d44eabbdaf7b1c93d1510cbf1a695d1d
Andrea Stagi <stagi.andrea@gmail.com>
2025-03-27 00:47:09 +0100

    chore: support multi entry build

--- rollup.config.js
+++ rollup.config.js
@@ -5,50 +5,68 @@ import replace from '@rollup/plugin-replace';
 import { litScss } from 'rollup-plugin-scss-lit';
 import sass from 'rollup-plugin-sass';
 import alias from '@rollup/plugin-alias';
+import multi from '@rollup/plugin-multi-entry';
 
 const GLOBAL_STYLESHEETS = ['**/global.scss']
 
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/index.bundle.js',
-    format: 'esm',
-  },
-  onwarn(warning) {
-    if (warning.code !== 'THIS_IS_UNDEFINED') {
-      console.error(`(!) ${warning.message}`);
+const PLUGINS_PER_ELEMENT = [
+  alias(
+    {
+      entries: [
+        { find: /\?inline$/, replacement: '' }
+      ]
     }
-  },
-  plugins: [
-    alias(
-      {
-        entries: [
-          { find: /\?inline$/, replacement: '' }
-        ]
-      }
-    ),
-    replace({preventAssignment: false, 'Reflect.decorate': 'undefined'}),
-    resolve(),
-    terser({
-      ecma: 2021,
-      module: true,
-      warnings: true,
-      mangle: {
-        properties: {
-          regex: /^__/,
-        },
+  ),
+  replace({preventAssignment: false, 'Reflect.decorate': 'undefined'}),
+  resolve(),
+  terser({
+    ecma: 2021,
+    module: true,
+    warnings: true,
+    mangle: {
+      properties: {
+        regex: /^__/,
       },
-    }),
-    litScss({
-      include: ['**/*.scss'],
-      minify: process.env.NODE_ENV === 'production',
-      options: { loadPaths: ['node_modules'] },
-      exclude: GLOBAL_STYLESHEETS
-    }),
-    sass({
-      output: 'dist/global.css',
-      include: GLOBAL_STYLESHEETS
-    }),
-    summary(),
-  ],
-};
+    },
+  }),
+  litScss({
+    include: ['**/*.scss'],
+    minify: process.env.NODE_ENV === 'production',
+    options: { loadPaths: ['node_modules'] },
+    exclude: GLOBAL_STYLESHEETS
+  }),
+  sass({
+    output: 'dist/global.css',
+    include: GLOBAL_STYLESHEETS
+  }),
+  summary(),
+]
+
+const ENTRY_FILES = [
+	{
+		input: 'src/button/button.js',
+		output: {
+			file: 'dist/button.js',
+			format: 'esm'
+		},
+    plugins: PLUGINS_PER_ELEMENT
+	},
+	{
+		input: 'src/input/input.js',
+		output: {
+			file: 'dist/input.js',
+			format: 'esm'
+		},
+    plugins: PLUGINS_PER_ELEMENT
+	},
+	{
+		input: 'src/icon/icon.js',
+		output: {
+			file: 'dist/icon.js',
+			format: 'esm'
+		},
+    plugins: PLUGINS_PER_ELEMENT
+	}
+];
+
+export default ENTRY_FILES
