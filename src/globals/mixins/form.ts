8e916c7f777491998030de8546091aa81a737ea5
Andrea Stagi <stagi.andrea@gmail.com>
2025-03-31 10:32:03 +0200

    chore: minfixes

--- src/globals/mixins/form.ts
+++ src/globals/mixins/form.ts
@@ -1,4 +1,13 @@
 /**
+ * @license
+ *
+ * Copyright IBM Corp. 2020, 2022
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
  * @param Base The base class.
  * @returns A mix-in to handle `formdata` event on the containing form.
  */
